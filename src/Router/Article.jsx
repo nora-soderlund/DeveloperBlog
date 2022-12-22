b64cbc694accbde616f405aded08c00027cae210
Nora Söderlund <norasoderlund@icloud.com>
2022-12-22 01:06:40 +0100

    Added article images

--- src/Router/Article.jsx
+++ src/Router/Article.jsx
@@ -36,8 +36,22 @@ export default class Article extends Component {
 
                 <p>I often see developers not understanding the importance of managing their Google Maps API key properly.</p>
                 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sapien lectus, ultricies quis euismod eu, gravida eu lacus. Cras luctus ipsum vel imperdiet commodo. Phasellus et libero in turpis congue feugiat. Nulla ultricies hendrerit lectus.</p>
+                <p contentEditable={true}>Think of it as a password. You wouldn't want to be throwing your password around to anybody, would you?</p>
+
+                <p contentEditable={true}>Essentially, an API key is a password - but it's even more powerful! You don't need an user name or e-mail to identify yourself with an API key, you only need the key.</p>
                 
+                <p contentEditable={true}>This is why it's good practice to always keep your key private. It should be straight-forward, right? Unfortunately it can be a bit confusing for newer developers because the context matters!</p>
+
+                <p contentEditable>If you're using the JavaScript API, Android SDK, iOS SDK, or the Embed API: then you must create a public API key and if you don't restrict it properly, you can end up with a massive bill from Google...</p>
+
+                <div className="article-images-3" style={{ height: "200px" }}>
+                    <img alt="" src="https://i.imgur.com/T2wJoYC.png"/>
+                    <img alt="" src="https://i.imgur.com/fsQufhF.png"/>
+                    <img alt="" src="https://i.imgur.com/5rnT0Ov.png"/>
+                </div>
+
+                <p contentEditable>edit</p>
+
                 <div className="article-code">
                     <SyntaxHighlighter language="javascript" style={vs2015} showLineNumbers={false} lineNumberContainerStyle={false}>
 {`function initMap() {
@@ -51,9 +65,9 @@ export default class Article extends Component {
                     <img alt="JavaScript" src={javascript}/>
                 </div>
 
-                <p>Nec sollicitudin sem rhoncus quis. Curabitur sed blandit ex, sit amet convallis dui. Aliquam pretium suscipit erat eget malesuada. Sed vitae pretium odio, non varius lorem. Maecenas feugiat ligula urna, in fringilla diam molestie nec. Morbi ut tincidunt erat.</p>
+                <p contentEditable={true}>Nec sollicitudin sem rhoncus quis. Curabitur sed blandit ex, sit amet convallis dui. Aliquam pretium suscipit erat eget malesuada. Sed vitae pretium odio, non varius lorem. Maecenas feugiat ligula urna, in fringilla diam molestie nec. Morbi ut tincidunt erat.</p>
                 
-                <p>Vivamus mollis turpis vel sapien sagittis porta. Praesent scelerisque nec velit id dictum. Donec facilisis libero nec felis convallis lacinia. Aenean commodo metus turpis, nec venenatis diam scelerisque vel. Sed velit nibh, dignissim ac ullamcorper sit amet, volutpat ac nisl. Quisque et tortor eu dolor ultrices sagittis non et ante.</p>
+                <p contentEditable={true}>Vivamus mollis turpis vel sapien sagittis porta. Praesent scelerisque nec velit id dictum. Donec facilisis libero nec felis convallis lacinia. Aenean commodo metus turpis, nec venenatis diam scelerisque vel. Sed velit nibh, dignissim ac ullamcorper sit amet, volutpat ac nisl. Quisque et tortor eu dolor ultrices sagittis non et ante.</p>
                 
                 <div className="article-code">
                     <SyntaxHighlighter language="html" style={vs2015} showLineNumbers={false} lineNumberContainerStyle={false}>
