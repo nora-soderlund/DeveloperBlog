8788ed76f1b3ae5a0a8a8b030f10da41927558fc
Nora Söderlund <nora.salehson@wow-group.com>
2022-12-23 12:44:42 +0100

    Removed obsolete resize event

--- src/Router/Article.jsx
+++ src/Router/Article.jsx
@@ -4,27 +4,6 @@ import { useParams } from "react-router";
 import Article from "../Components/Article";
 
 class ArticleRouterComponent extends Component {
-    componentDidMount() {
-        window.addEventListener("resize", () => this.onResize());
-
-        this.onResize();
-    };
-
-    onResize() {
-        const elements = document.getElementsByClassName("article-code");
-
-        for(let index = 0; index < elements.length; index++)
-            elements[index].style.width = "0px";
-
-        for(let index = 0; index < elements.length; index++) {
-            const parent = elements[index].parentElement;
-
-            const width = parent.getBoundingClientRect().width;
-
-            elements[index].style.width = `${width}px`;
-        }
-    };
-
     render() {
         return (
             <Article slug={this.props.params.slug}/>
