8dccbf61fa9968cf977e1365637c2b6a5de457e5
Nora Söderlund <norasoderlund@icloud.com>
2022-12-26 20:05:28 +0100

    Added fire and wrench icon

--- src/Components/Icons.jsx
+++ src/Components/Icons.jsx
@@ -1,8 +1,8 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faThumbsUp as farThumbsUp, faThumbsDown as farThumbsDown } from "@fortawesome/free-regular-svg-icons";
-import { faThumbsUp as fasThumbsUp, faThumbsDown as fasThumbsDown, faLink as fasLink } from "@fortawesome/free-solid-svg-icons";
+import { faThumbsUp as fasThumbsUp, faThumbsDown as fasThumbsDown, faLink as fasLink, faWrench as fasWrench, faFire as fasFire } from "@fortawesome/free-solid-svg-icons";
 import { faGithub as fabGithub, faStackOverflow as fabStackOverflow, faLinkedin as fabLinkedin, faInstagram as fabInstagram, faGoogle as fabGoogle } from "@fortawesome/free-brands-svg-icons";
 
 library.add(farThumbsUp, farThumbsDown);
-library.add(fasThumbsUp, fasThumbsDown, fasLink);
+library.add(fasThumbsUp, fasThumbsDown, fasLink, fasWrench, fasFire);
 library.add(fabGithub, fabStackOverflow, fabLinkedin, fabInstagram, fabGoogle);
