0dc744a32c47b3906ccf6242a2155dffa155f9ca
Nora Söderlund <norasoderlund@icloud.com>
2022-12-26 21:36:44 +0100

    Added brand colors on hover

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -6,15 +6,15 @@ export default class Footer extends Component {
         return (
             <footer>
                 <div className="footer-links">
-                    <a href="https://www.linkedin.com/in/nora-soderlund/" target="_blank" rel="noreferrer">
+                    <a class="footer-link-linkedin" href="https://www.linkedin.com/in/nora-soderlund/" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "linkedin"]}/>
                     </a>
 
-                    <a href="https://github.com/nora-soderlund" target="_blank" rel="noreferrer">
+                    <a class="footer-link-github" href="https://github.com/nora-soderlund" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "github"]}/>
                     </a>
                     
-                    <a href="https://stackoverflow.com/users/20073186/nora-s%c3%b6derlund" target="_blank" rel="noreferrer">
+                    <a class="footer-link-stack-overflow" href="https://stackoverflow.com/users/20073186/nora-s%c3%b6derlund" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "stack-overflow"]}/>
                     </a>
                 </div>
