a8a68c4b3e7d7b7e49b3a387434cf7b1b7d32e02
Nora Söderlund <norasoderlund@icloud.com>
2022-12-27 00:01:33 +0100

    Tidied up print style

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -8,14 +8,20 @@ export default class Footer extends Component {
                 <div className="footer-links">
                     <a class="footer-link-linkedin" href="https://www.linkedin.com/in/nora-soderlund/" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "linkedin"]}/>
+
+                        <span className="print-only">Nora Söderlund</span>
                     </a>
 
                     <a class="footer-link-github" href="https://github.com/nora-soderlund" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "github"]}/>
+
+                        <span className="print-only">nora-soderlund</span>
                     </a>
                     
                     <a class="footer-link-stack-overflow" href="https://stackoverflow.com/users/20073186/nora-s%c3%b6derlund" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "stack-overflow"]}/>
+
+                        <span className="print-only">Nora Söderlund</span>
                     </a>
                 </div>
             </footer>
