e00ce2e09450f59343a8dd827c2dcdb68172d527
Nora Söderlund <norasoderlund@icloud.com>
2022-12-26 19:40:17 +0100

    Added support for pagination

--- src/Router/Index.jsx
+++ src/Router/Index.jsx
@@ -7,9 +7,15 @@ export default class Index extends Component {
     componentDidMount() {
         document.title = App.title;
 
-        fetch(`${process.env.REACT_APP_API ?? ""}/api/v1/articles`)
+        fetch(`${process.env.REACT_APP_API ?? ""}/api/v1/articles?start=${this.state?.start ?? 0}`)
             .then((response) => response.json())
-            .then((result) => this.setState({ articles: result }));
+            .then((result) => this.setState({ start: result.end, articles: result.articles, paginatable: result.paginatable }));
+    };
+
+    onPaginate() {
+        fetch(`${process.env.REACT_APP_API ?? ""}/api/v1/articles?start=${this.state?.start ?? 0}`)
+            .then((response) => response.json())
+            .then((result) => this.setState({ start: result.end, articles: this.state.articles.concat(result.articles), paginatable: result.paginatable }));
     };
 
     render() {
@@ -18,6 +24,10 @@ export default class Index extends Component {
                 {(!this.state || !this.state.articles)?(
                     <Article compact/>
                 ):(this.state.articles.map((slug) => (<Article key={slug} slug={slug} compact/>)))}
+
+                {(this.state?.paginatable) && (
+                    <p className="articles-paginate" onClick={() => this.onPaginate()}>Load more articles</p>
+                )}
             </div>
         );
     };
