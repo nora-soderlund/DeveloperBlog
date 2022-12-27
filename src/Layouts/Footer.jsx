212f52aa73e8721bef61be40c3437c2e93f6693f
Nora Söderlund <norasoderlund@icloud.com>
2022-12-27 03:51:46 +0100

    Fixed class > className in footer

--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -6,19 +6,19 @@ export default class Footer extends Component {
         return (
             <footer>
                 <div className="footer-links">
-                    <a class="footer-link-linkedin" href="https://www.linkedin.com/in/nora-soderlund/" target="_blank" rel="noreferrer">
+                    <a className="footer-link-linkedin" href="https://www.linkedin.com/in/nora-soderlund/" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "linkedin"]}/>
 
                         <span className="print-only">Nora Söderlund</span>
                     </a>
 
-                    <a class="footer-link-github" href="https://github.com/nora-soderlund" target="_blank" rel="noreferrer">
+                    <a className="footer-link-github" href="https://github.com/nora-soderlund" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "github"]}/>
 
                         <span className="print-only">nora-soderlund</span>
                     </a>
                     
-                    <a class="footer-link-stack-overflow" href="https://stackoverflow.com/users/20073186/nora-s%c3%b6derlund" target="_blank" rel="noreferrer">
+                    <a className="footer-link-stack-overflow" href="https://stackoverflow.com/users/20073186/nora-s%c3%b6derlund" target="_blank" rel="noreferrer">
                         <FontAwesomeIcon icon={["fab", "stack-overflow"]}/>
 
                         <span className="print-only">Nora Söderlund</span>
