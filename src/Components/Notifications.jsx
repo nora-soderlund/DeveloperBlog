5ceffe78896693daa8e6aede315fc5a7618d73e8
Nora Söderlund <norasoderlund@icloud.com>
2023-01-08 01:04:24 +0100

    Fixed issue with notifications

--- src/Components/Notifications.jsx
+++ src/Components/Notifications.jsx
@@ -25,6 +25,9 @@ export default class Notifications extends Component {
 
         // TODO: add a fade out animation 
         setTimeout(() => {
+            if(!this.state || !this.state.notifications)
+                return;
+
             if(this.state.notifications.find((_notification) => _notification.id === notification.id))
                 this.setState({ notifications: this.state.notifications.filter((_notification) => _notification.id != notification.id) });
         }, 2000);
