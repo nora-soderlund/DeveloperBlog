eb34b39aabe794ca132462af8b11788b59818d31
Nora Söderlund <norasoderlund@icloud.com>
2023-01-09 18:58:49 +0100

    Fixed recurring issue with database connection

--- src/Services/Database.jsx
+++ src/Services/Database.jsx
@@ -3,33 +3,53 @@ import mysql from "mysql";
 import config from "../../config.json" assert { type: "json" };
 
 export default class Database {
+    static connecting = null;
     static connection = null;
 
-    static connect() {
-        return new Promise((resolve) => {
-            if(this.connection !== null && this.connection.state !== "disconnected")
-                resolve({ error: null });
+    static connectAsync() {
+        // we return promises with no real usage because we still expect a promise even if we don't need one
+        // this is because we want to intercept new connections as they happen during another connection
 
+        if(this.connection !== null && this.connection.state !== "disconnected")
+            return new Promise((resolve) => resolve({ error: null }));
+
+        // if we're already connecting, we'll return the same promise, see comments above
+        if(this.connecting !== null)
+            return this.connecting;
+
+        this.connecting = new Promise((resolve) => {
             this.connection = mysql.createConnection(config.database);
     
             this.connection.connect((error) => {
+                this.connecting = null;
+
                 resolve({ error });
             });
         });
+
+        return this.connecting;
+    };
+
+    static async ensureConnectionAsync() {
+        if(this.connection === null || this.connection.state === "disconnected") {
+            const { error } = await this.connectAsync();
+
+            if(error)
+                throw new Error(error);
+        }
     };
 
     static escape(input) {
         if(this.connection === null || this.connection.state === "disconnected")
-            this.connect();
-            
+            throw new Error("You must call Database.ensureConnectionAsync() before calling Database.escape()");
+
         return this.connection.escape(input);
     };
 
     static async queryAsync(query) {
-        return new Promise((resolve) => {
-            if(this.connection === null || this.connection.state === "disconnected")
-                this.connect();
+        await this.ensureConnectionAsync();
 
+        return new Promise(async (resolve) => {
             this.connection.query(query, (error, rows) => {
                 if(error)
                     resolve({ error, rows });
