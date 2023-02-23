5c7eef404c86001896cef23aaff5d3123447bd35
Nora Söderlund <norasoderlund@icloud.com>
2023-02-23 18:18:01 +0100

    Fixed Github activity query

--- src/Services/Database/Github.ts
+++ src/Services/Database/Github.ts
@@ -6,7 +6,7 @@ export default class Github {
     static async getGithubNotifications(): Promise<GithubNotification[]> {
         const connection = await Database.ensureConnectionAsync();
 
-        const { error, rows } = await Database.queryAsync(connection, `SELECT * FROM github WHERE reason != "review_requested" ORDER BY updated_at DESC LIMIT 3`);
+        const { error, rows } = await Database.queryAsync(connection, `SELECT * FROM github WHERE reason != "review_requested" AND repository_url != null AND subject_url != null ORDER BY updated_at DESC LIMIT 3`);
         
         connection.destroy();
         
